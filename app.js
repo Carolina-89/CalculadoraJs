ab477c386a6a81f7d0daef73704e73bf165f961f
Karo <48401511+Carolina-89@users.noreply.github.com>
2021-11-08 12:58:55 -0300

    1er commit
    html, css & js

---
+++ app.js
@@ -0,0 +1,51 @@
+function num7() {
+    document.calculadora.ans.value+='7'
+}
+
+function num8() {
+    document.calculadora.ans.value+='8'
+}
+
+function suma() {
+    document.calculadora.ans.value+='+'
+}
+
+function num4() {
+    document.calculadora.ans.value+='4'
+}
+
+function num5() {
+    document.calculadora.ans.value+='5'
+}
+
+function num6() {
+    document.calculadora.ans.value+='6'
+}
+
+function resta() {
+    document.calculadora.ans.value+='-'
+}
+
+function num1() {
+    document.calculadora.ans.value+='1'
+}
+
+function num2() {
+    document.calculadora.ans.value+='2'
+}
+
+function num3() {
+    document.calculadora.ans.value+='3'
+}
+
+function multiplica() {
+    document.calculadora.ans.value+='*'
+}
+
+function num0() {
+    document.calculadora.ans.value+='0'
+}
+
+function divide() {
+    document.calculadora.ans.value+='/'
+}
